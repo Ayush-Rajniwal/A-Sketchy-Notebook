6c36fd1ddf70df5ffa826d366a14b607d3412093
Ayush Rajniwal <ayush.rajniwal@gmail.com>
2020-05-28 16:33:20 +0530

    Initial commit

---
+++ src/components/MainComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class Main extends Component {
+    state = {  }
+    render() { 
+        return (
+          <div className="jumbotron">
+            <h1>Ready</h1>
+          </div>
+        );
+    }
+}
+ 
+export default Main;
